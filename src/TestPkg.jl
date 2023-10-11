96c8b986b7e51ce86a134eff9f1b7ab395be5fbd
Bruno Amorim <amorim.bac@gmail.com>
2023-10-11 10:57:32 +0100

    add function

--- src/TestPkg.jl
+++ src/TestPkg.jl
@@ -1,5 +1,11 @@
 module TestPkg
 
-# Write your package code here.
+using IJulia
+
+export add_kernel
+
+function add_kernel(t)
+    installkernel("julia_$(t)_threads", env=Dict("JULIA_NUM_THREADS"=>"$t"))
+end
 
 end
